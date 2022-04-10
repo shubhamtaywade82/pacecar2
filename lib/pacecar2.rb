f3f9bbf6ee75bbfe8ee7bb758b4c62c349234840
Shubham Taywade <shubhamtaywade82@gmail.com>
2022-04-10 15:20:28 +0530

    Initial Commit - Create Gem

---
+++ lib/pacecar2.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pacecar2/version"
+
+module Pacecar2
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/pacecar2_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Pacecar2 do
+  it "has a version number" do
+    expect(Pacecar2::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
